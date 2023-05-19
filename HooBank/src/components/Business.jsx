72f441d140f2144e4227bc68fcec6d1d8bf23152
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2023-05-19 15:51:06 -0300

    up

--- HooBank/src/components/Business.jsx
+++ HooBank/src/components/Business.jsx
@@ -5,8 +5,11 @@ import Button from './Button';
 export const Business = () => (
     <section id="features" className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2 className={styles.heading2}>Você faz o negócio, <br className="sm:block hidden" /> nós cuidamons do dinheiro.</h2>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Com o cartão de crédito certo, você pode melhorar sua vida financeira acumulando crédito, ganhando recompensas e economizando dinheiro. Mas com centenas de cartões de crédito no mercado.</p>
+        <h2 className={styles.heading2}>Você faz o negócio, <br className="sm:block hidden" /> nós cuidamos do dinheiro.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Com o cartão de crédito certo, 
+        você pode melhorar sua vida financeira acumulando crédito, 
+        ganhando recompensas e economizando dinheiro. 
+        Mas com centenas de cartões de crédito no mercado.</p>
       </div>
     </section>
 )
